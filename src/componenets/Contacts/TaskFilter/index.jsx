a4c82566ba36987058693ba99a8ac8c59ae875d7
irem acar <iremmacar35@gmail.com>
2024-07-16 22:08:36 +0300

    tasks commit

--- src/componenets/Contacts/TaskFilter/index.jsx
+++ src/componenets/Contacts/TaskFilter/index.jsx
@@ -15,8 +15,7 @@ function TaskFilter({ filterText, setFilterText, filterPeriod }) {
         <Dropdown.Toggle variant="success" id="dropdown-basic">
           Period
         </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => filterPeriod("weekly")}>Weekly</Dropdown.Item>
+        <Dropdown.Menu><Dropdown.Item onClick={() => filterPeriod("weekly")}>Weekly</Dropdown.Item>
           <Dropdown.Item onClick={() => filterPeriod("monthly")}>Monthly</Dropdown.Item>
           <Dropdown.Item onClick={() => filterPeriod("yearly")}>Yearly</Dropdown.Item>
           <Dropdown.Item onClick={() => filterPeriod("all")}>All</Dropdown.Item>
